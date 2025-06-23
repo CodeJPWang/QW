417afd2298c7468d6ee36c3278f535cd5bc175ad
Jiping Wang <129336984+CodeJPWang@users.noreply.github.com>
2025-06-23 19:07:28 +0100

    Update autoload.js

--- assets/js/autoload.js
+++ assets/js/autoload.js
@@ -1,5 +1,7 @@
-
 async function includeFragments () {
+  if (window._fragmentsIncluded) return;
+  window._fragmentsIncluded = true;
+
   const nodes = [...document.querySelectorAll('[data-include]')];
   await Promise.all(
     nodes.map(async n => {
@@ -9,4 +11,6 @@ async function includeFragments () {
   );
   document.dispatchEvent(new CustomEvent('partialsReady'));
 }
+
+// 第一次调用，函数内部判断为 false，执行并设置 flag
 includeFragments();
